2cd5e1e590462addb3043663247a5a16137661ef
Diego Coscolla <diegocoscolla@gmail.com>
2023-04-01 00:14:29 -0300

    Add posts router

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,16 @@
 import { type NextPage } from "next";
 import Head from "next/head";
 
-import { SignIn, SignInButton, SignOutButton, useUser } from "@clerk/nextjs";
+import { SignInButton, SignOutButton, useUser } from "@clerk/nextjs";
+import { api } from "@/utils/api";
 
 const Home: NextPage = () => {
   const user = useUser();
+
+  const { data } = api.posts.getAll.useQuery();
+
+  console.log(data);
+
   return (
     <>
       <Head>
@@ -15,7 +21,12 @@ const Home: NextPage = () => {
       <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c]">
         <div>{!user.isSignedIn && <SignInButton />}</div>
         <div>{user.isSignedIn && <SignOutButton />}</div>
-        <SignIn path="/sign-in" routing="path" signUpUrl="/sign-up" />
+
+        <div>
+          {data?.map((post) => (
+            <div key={post.id}>{post.content}</div>
+          ))}
+        </div>
       </main>
     </>
   );
